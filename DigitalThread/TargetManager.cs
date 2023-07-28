c0d107235be9531476cac426eb2981fa43f09f7d
Steve <stephen.r.strong@saic.com>
2023-07-27 23:00:38 -0400

    ij

--- DigitalThread/TargetManager.cs
+++ DigitalThread/TargetManager.cs
@@ -19,33 +19,90 @@ namespace Visio2023Foundry.Targets;
 
 public class TargetManager : FoWorkbook
 {
+    private FoMenu2D? TargetMenu { get; set; }
     private DT_System? SystemNetwork { get; set; }
-    private IDrawing Drawing  { get; set; }
+    private IDrawing Drawing { get; set; }
     private FoLayoutNetwork<ThreadShape1D, ThreadShape2D> NetworkLayout { get; set; } = new();
 
     //private Dictionary<string,DT_Target> ModelLookup { get; set; } = new();
 
-    public TargetManager(IWorkspace space, IFoundryService foundry): 
-        base(space,foundry)
+    public TargetManager(IWorkspace space, IFoundryService foundry) :
+        base(space, foundry)
     {
         Drawing = space.GetDrawing()!;
     }
 
 
+    private void CreateMenu2D()
+    {
+        var drawing = Workspace.GetDrawing();
+        if ( drawing == null || TargetMenu == null) return;
 
+        TargetMenu.ToggleLayout();
+        drawing.AddShape<FoMenu2D>(TargetMenu).AnimatedMoveTo(900, 100);
+    }
 
     public override void CreateMenus(IWorkspace space, IJSRuntime js, NavigationManager nav)
     {
         "TargetManager".WriteWarning();
         var menu = new Dictionary<string, Action>()
         {
-            { "Testing", () => DoTesting() },
+            { "Clear", () => DoClear() },
+            { "Create", () => DoCreate() },
+            { "Step 1", () => DoStep1() },
+            { "Step 5", () => DoStep5() },
+            { "Attract", () => DoAttract() },
+            { "Repel", () => DoRepel() },
+            { "Center", () => DoCenter() },
+            { "Boundry", () => DoBoundry() },
         };
-        
-        space.EstablishMenu2D<FoMenu2D, FoButton2D>("Targets", menu, true);
+
+        TargetMenu = space.EstablishMenu2D<FoMenu2D, FoButton2D>("Targets", menu, true);
+        CreateMenu2D();
     }
 
+    private void DoClear()
+    {
+        var drawing = Workspace.GetDrawing();
+        if (drawing == null) return;
+        drawing.ClearAll();
+        CreateMenu2D();
+    }
 
+    private void DoCreate()
+    {
+        DoTesting();
+    }
+
+    private void DoStep1()
+    {
+        NetworkLayout.DoIteration(1);
+    }
+
+    private void DoStep5()
+    {
+        NetworkLayout.DoIteration(5);
+    }
+
+    private void DoAttract()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DoRepel()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DoCenter()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DoBoundry()
+    {
+        throw new NotImplementedException();
+    }
 
     public override void PreRender(int tick)
     {
@@ -53,7 +110,7 @@ public class TargetManager : FoWorkbook
     }
     public override async Task RenderWatermark(Canvas2DContext ctx, int tick)
     {
-          await NetworkLayout.RenderLayoutNetwork(ctx,tick);
+        await NetworkLayout.RenderLayoutNetwork(ctx, tick);
     }
 
     public FoLayoutNode<V> CreateNodeShape<V>(DT_Target model, Action<V, DT_Target> TagAction) where V : ThreadShape2D
@@ -69,11 +126,11 @@ public class TargetManager : FoWorkbook
 
         Random rnd = new();
 
-        var node = new FoLayoutNode<V>(shape, 1000* rnd.NextDouble(), 1000 * rnd.NextDouble());
+        var node = new FoLayoutNode<V>(shape, 1000 * rnd.NextDouble(), 1000 * rnd.NextDouble());
         return node;
     }
 
-    public FoLayoutLink<U,V> CreateLinkShape<U,V>(DT_TargetLink model, Action<U, DT_TargetLink> TagAction) where U : ThreadShape1D where V : ThreadShape2D
+    public FoLayoutLink<U, V> CreateLinkShape<U, V>(DT_TargetLink model, Action<U, DT_TargetLink> TagAction) where U : ThreadShape1D where V : ThreadShape2D
     {
 
         var shape = Activator.CreateInstance<U>();
@@ -81,7 +138,7 @@ public class TargetManager : FoWorkbook
 
         TagAction?.Invoke(shape, model);
         Drawing?.AddShape<U>(shape);
-        var link = new FoLayoutLink<U,V>(shape);
+        var link = new FoLayoutLink<U, V>(shape);
         return link;
     }
 
@@ -90,9 +147,9 @@ public class TargetManager : FoWorkbook
     {
         var system = new DT_System();
 
-        var t0 = system.CreateTarget("PIN","10");
-        var t1 = system.CreateTarget("ASST","20");
-        var t2= system.CreateTarget("PROC","30");
+        var t0 = system.CreateTarget("PIN", "10");
+        var t1 = system.CreateTarget("ASST", "20");
+        var t2 = system.CreateTarget("PROC", "30");
 
         system.CreateLink(t0, t1);
         system.CreateLink(t1, t2);
@@ -131,7 +188,7 @@ public class TargetManager : FoWorkbook
             if (source != null && sink != null)
                 link.Connect(source, sink);
         }
-            
+
 
 
 
